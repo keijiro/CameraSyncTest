9e8afe7fdce9e6012ca23942d47d70c076cec795
Keijiro Takahashi <keijiro@Odyssey.jt.unity3d.com>
2015-04-23 19:35:18 +0900

    Initial commit.

---
+++ Assets/FlipFlop.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlipFlop : MonoBehaviour
+{
+    IEnumerator Start()
+    {
+        var r = GetComponent<Renderer>();
+
+        while (true)
+        {
+            r.enabled = true;
+            yield return null;
+
+            r.enabled = false;
+            yield return null;
+        }
+    }
+}
